88e48c3af2255379409b524ed2ecb391c00b3407
sajo sam <saajosaam@gmail.com>
2021-05-31 00:12:57 +0530

    search func and admin

--- cars/urls.py
+++ cars/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search')
  
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from cars import models
 from cars.models import Car
 from django.shortcuts import render
 from .models import Team
@@ -8,10 +9,21 @@ def home(request):
     teams=Team.objects.all()
     featured_cars=Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars=Car.objects.order_by('-created_date')
+    model_search=Car.objects.values_list('model', flat=True).distinct()
+    city_search=Car.objects.values_list('city', flat=True).distinct()
+    year_search=Car.objects.values_list('year', flat=True).distinct()
+    body_style_search=Car.objects.values_list('body_style', flat=True).distinct()
+
+
     data={
         'teams':teams,
         'featured_cars':featured_cars,
         'all_cars':all_cars,
+        'model_search':model_search,
+        'city_search':city_search,
+        'year_search':year_search,
+        'body_style_search':body_style_search,
+
 
     }
     return render(request, 'pages/home.html', data)
