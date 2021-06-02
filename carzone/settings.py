6a1c16b61a4890eda2b6e5bbc7ae028c7134f512
sajo sam <saajosaam@gmail.com>
2021-06-02 13:22:20 +0530

    fixing runtime

--- carzone/settings.py
+++ carzone/settings.py
@@ -144,18 +144,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-
-STATICFILES_DIRS = [
-    BASE_DIR / "carzone/static",
-]
-
 # STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'carzone/static'),
+#     BASE_DIR / "carzone/static",
 # ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'carzone/static'),
+]
 
 # MEDIA_ROOT = BASE_DIR /'media/'
 # MEDIA_URL='/media/'
